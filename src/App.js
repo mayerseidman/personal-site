c78967ff0d1cfa852ffbf595684a7bb7593b94c7
mayerseidman <mzseidman@gmail.com>
2019-10-01 10:11:33 -0700

    initializes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// img src0{logo}
+
+function App() {
+    return (
+        <div className="app">
+            <div className="leftContainer"></div>
+            <div className="rightContainer"></div>
+        </div>
+    );
+}
+export default App;
