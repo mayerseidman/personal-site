a186a768b2f5ea37bcfe716b92ba5ae737c984bb
mayerseidman <mzseidman@gmail.com>
2019-10-02 10:04:07 -0700

    adds content and mobile css home page also scss

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-// img src0{logo}
+// img src={logo}
 
 function App() {
     return (
-        <div className="app">
-            <div className="leftContainer"></div>
+        <div className="portfolioContainer">
+            <div className="leftContainer">
+            	<div className="infoContainer">
+            		<p className="name bigText">Mayer Seidman</p>
+            		<p className="title bigText">Designer <span className="circleDivider"></span> Developer</p>
+            	</div>
+            </div>
             <div className="rightContainer"></div>
         </div>
     );
